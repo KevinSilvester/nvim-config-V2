9455499749624cbf1702b373ebd63859c3d27e75
KevinSilvester <kevinsilvester05@gmail.com>
2022-10-03 20:28:16 +0100

    feat(ui): updated plugins + config, setting up statusline

--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -1,26 +1,26 @@
 local plugin = require("core.pack").register_plugin
 local conf = require("modules.ui.config")
 
-plugin({ "glepnir/zephyr-nvim", config = conf.zephyr })
+plugin({ "glepnir/zephyr-nvim" })
+plugin({ "folke/tokyonight.nvim" })
+plugin({ "lunarvim/darkplus.nvim" })
+plugin({ "lunarvim/onedarker.nvim" })
+plugin({ "rebelot/kanagawa.nvim" })
+plugin({ "marko-cerovac/material.nvim" })
+plugin({ "olimorris/onedarkpro.nvim" })
 
-plugin({ "glepnir/dashboard-nvim", config = conf.dashboard })
+plugin({ "goolord/alpha-nvim", config = conf.dashboard, requires = "kyazdani42/nvim-web-devicons" })
 
-plugin({
-   "glepnir/galaxyline.nvim",
-   branch = "main",
-   config = conf.galaxyline,
-   requires = "kyazdani42/nvim-web-devicons",
-})
+plugin({ "feline-nvim/feline.nvim", requires = "kyazdani42/nvim-web-devicons" })
 
 plugin({
    "kyazdani42/nvim-tree.lua",
-   cmd = "NvimTreeToggle",
-   config = conf.nvim_tree,
+   -- config = conf.nvim_tree,
    requires = "kyazdani42/nvim-web-devicons",
 })
 
 plugin({
    "akinsho/nvim-bufferline.lua",
-   config = conf.nvim_bufferline,
+   config = conf.bufferline,
    requires = "kyazdani42/nvim-web-devicons",
 })
